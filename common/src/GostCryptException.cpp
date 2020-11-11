efc0d36374c0581ac897a18fc252c4d000695b50
badbouille <14301545+badbouille@users.noreply.github.com>
2020-11-11 15:50:30 +0100

    [#3] Noticing an error in GostCryptException. Quickfixed the problem but will need a better fix later.

--- common/src/GostCryptException.cpp
+++ common/src/GostCryptException.cpp
@@ -2,6 +2,7 @@
 #include "GostCryptException.h"
 
 #include <sstream>
+#include <cstring>
 
 const char* GostCrypt::GostCryptException::what() const noexcept {
 	std::stringstream output;
@@ -10,5 +11,14 @@ const char* GostCrypt::GostCryptException::what() const noexcept {
 	output << "::" << func;
 	output << "::" << l;
 	output << " \t" << msg;
-	return output.str().c_str();
+
+	// Note: Exception::what() should not build a message but instead should have a predefined message ready to be sent.
+	// In GostCrypt, we usually need a lot of parameters and building the exception here is VERY practical.
+	// The huge drawback is that the ressource is never freed.
+	// TODO: move this code in the Constructor maybe, so this function returns only a pointer.
+
+	char *message = new char[strlen(output.str().c_str())+1];
+	strcpy(message, output.str().c_str());
+
+	return message;
 }
