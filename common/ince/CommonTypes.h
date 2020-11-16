41fa7a693e9a88fcc2bf56d3ba77e9245eacf4af
badbouille <14301545+badbouille@users.noreply.github.com>
2020-11-16 21:42:53 +0100

    Adding a simple type definition file, containing standard sizes for now.

---
+++ common/ince/CommonTypes.h
@@ -0,0 +1,18 @@
+//
+// Created by badbouille on 16/11/2020.
+//
+
+#ifndef GOSTCRYPT_COMMONTYPES_H
+#define GOSTCRYPT_COMMONTYPES_H
+
+/* Constants and Defines */
+
+/* Sizes (use parenthesis) */
+#define  B 1
+#define KB 1024
+#define MB 1024*1024
+#define GB 1024*1024*1024
+#define TB 1024*1024*1024*1024
+
+
+#endif //GOSTCRYPT_COMMONTYPES_H
