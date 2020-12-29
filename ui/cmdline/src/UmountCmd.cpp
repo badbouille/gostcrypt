42728401249bfe4f87ca5dfdfe37ca62fe3d6445
badbouille <14301545+badbouille@users.noreply.github.com>
2020-12-29 23:44:17 +0100

    Adding -a option to umount in command line, bound to umountAll

--- ui/cmdline/src/UmountCmd.cpp
+++ ui/cmdline/src/UmountCmd.cpp
@@ -20,23 +20,32 @@ static char args_doc_umount[] = "FOLDER";
 
 /* Options */
 static struct argp_option umount_options[] = {
+        {"all", 'a', nullptr, 0, "Unmount all volumes. Do not give a folder name when using this option." },
         {nullptr }
+};
+
+struct UmountOptions_s {
+    std::string mountpoint;
+    bool all;
 };
 
 /* Custom parser */
 static error_t parse_opt_umount (int key, char *arg, struct argp_state *state) {
     /* Get the input argument from argp_parse, which we
        know is a pointer to our arguments structure. */
-    std::string *arguments = (std::string *)state->input;
+    struct UmountOptions_s *arguments = (struct UmountOptions_s *)state->input;
 
     switch (key)
     {
+        case 'a':
+            arguments->all = true;
+            break;
         case ARGP_KEY_NO_ARGS:
             argp_usage (state);
             break;
         case ARGP_KEY_ARG:
-            if (state->arg_num == 0) {
-                *arguments = std::string(arg);
+            if (state->arg_num == 0 && !(arguments->all)) {
+                arguments->mountpoint = std::string(arg);
                 break;
             }
             /* Too many arguments. */
@@ -59,17 +68,22 @@ static struct argp argp_umount = { umount_options, parse_opt_umount, args_doc_um
 
 /* Real command */
 int cmd_umount(int argc, char **argv) {
-    std::string folder;
+    struct UmountOptions_s opts;
     SecureBuffer pass(64);
 
-    folder = "";
+    opts.mountpoint = "";
+    opts.all = false;
 
-    argp_parse (&argp_umount, argc, argv, 0, 0, &folder);
+    argp_parse (&argp_umount, argc, argv, 0, 0, &opts);
 
-    std::cout << "Unmounting folder: " << folder << std::endl;
+    std::cout << "Unmounting folder: " << opts.mountpoint << std::endl;
 
     try {
-        Core::umount(folder);
+        if (opts.all) {
+            Core::umountAll();
+        } else {
+            Core::umount(opts.mountpoint);
+        }
     } catch (GostCryptException &e) {
         std::cout << "Cannot unmount volume. " << e.what() << std::endl;
     }
