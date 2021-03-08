ac3c3815f8af1cbbf3fb4916a1b5a699841fcc70
badbouille <14301545+badbouille@users.noreply.github.com>
2021-03-08 23:03:29 +0100

    Implementing PRNGSystem for windows using rand_s.
    Note: rand_s may not be fast enough and too expensive considering the current use of PRNGSystem..

--- src/crypto/ince/PRNGSystem.h
+++ src/crypto/ince/PRNGSystem.h
@@ -9,6 +9,11 @@
 #ifndef _PRNGSYSTEM_H
 #define _PRNGSYSTEM_H
 
+#ifdef PLATFORM_WINDOWS
+#define _CRT_RAND_S
+#include <stdlib.h>
+#endif
+
 #include <Buffer.h>
 #include <fstream>
 #include "PRNG.h"
@@ -26,11 +31,13 @@ namespace GostCrypt
         std::string GetName() const override { return "System PRNG"; };
         std::string GetID() const override { return "SPRNG"; };
         std::string GetDescription() const override { return "The System PRNG extracts random values from the pseudo-random "
-                                                            "generator of the current system, (/dev/urandom on linux). "
+                                                            "generator of the current system, (/dev/urandom on linux, rand_s on windows). "
                                                             "This may not be a secure way of generating random numbers, "
                                                             "but a very fast way of generating random data across the disk."; };
+#ifdef PLATFORM_LINUX
     protected:
         std::ifstream randsource;
+#endif
     };
 
 }
--- src/crypto/src/PRNGSystem.cpp
+++ src/crypto/src/PRNGSystem.cpp
@@ -8,6 +8,9 @@
 
 #include "PRNGSystem.h"
 
+/* Linux implementation using "/dev/urandom" */
+#ifdef PLATFORM_LINUX
+
 #define RANDOM_FILE "/dev/urandom"
 
 GostCrypt::PRNGSystem::PRNGSystem()
@@ -33,3 +36,44 @@ void GostCrypt::PRNGSystem::Get(GostCrypt::SecureBufferPtr &data)
     }
     randsource.read((char *)data.get(), data.size());
 }
+
+#endif
+
+/* Windows implementation using rand_s */
+#ifdef PLATFORM_WINDOWS
+
+GostCrypt::PRNGSystem::PRNGSystem() {}
+
+GostCrypt::PRNGSystem::~PRNGSystem() {}
+
+void GostCrypt::PRNGSystem::Get(GostCrypt::SecureBufferPtr &data)
+{
+    uint32_t i = 0;
+    uint32_t randomvalue;
+    uint8_t randomvalue8[4];
+    if (!(((size_t)data.get()) & 0x03)) { // alignement
+        for ( ; i < (data.size()/4); i++ ) {
+            if (rand_s(&randomvalue) != 0) {
+                throw GOSTCRYPTEXCEPTION("rand_s failed to acquire data");
+            }
+            ((uint32_t *)data.get())[i] = randomvalue;
+        }
+        i*=4;
+    }
+    for ( ; i < data.size(); i+=4 ) {
+        if ( (i%4 == 0) && (rand_s((unsigned int *)&randomvalue8) != 0)) {
+            throw GOSTCRYPTEXCEPTION("rand_s failed to acquire data");
+        }
+        data.get()[i  ] = randomvalue8[0];
+        if (i+1 >= data.size()) break;
+        data.get()[i+1] = randomvalue8[0];
+        if (i+2 >= data.size()) break;
+        data.get()[i+2] = randomvalue8[0];
+        if (i+3 >= data.size()) break;
+        data.get()[i+3] = randomvalue8[0];
+    }
+    randomvalue = 0;
+    randomvalue8[0] = randomvalue8[1] = randomvalue8[2] = randomvalue8[3] = 0;
+}
+
+#endif
